35b04f6943859509bb73fed756259f7cdfe0fc26
zoudong <zhoud006@udduo.com>
2018-06-04 17:10:55 +0800

    package

---
+++ src/main/java/com/zoudong/Test.java
@@ -0,0 +1,9 @@
+package com.zoudong;
+
+/**
+ * @author zd
+ * @description class
+ * @date 2018/6/4 17:10
+ */
+public class Test {
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,7 @@
+/**
+ * @author zd
+ * @description class
+ * @date 2018/6/4 17:10
+ */
+public class Test {
+}
