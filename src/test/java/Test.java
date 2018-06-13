66fbb6d0d311493a3d984c94e28a82727993e295
zoudong <zhoud006@udduo.com>
2018-06-13 18:08:02 +0800

    test

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,7 +1,22 @@
+import com.zoudong.permission.mapper.TestMapper;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 /**
  * @author zd
  * @description class
  * @date 2018/6/4 17:10
  */
+@RunWith(SpringRunner.class)
 public class Test {
+    @Autowired
+    private TestMapper testMapper;
+    @org.junit.Test
+    public void test(){
+        com.zoudong.permission.model.Test test=new com.zoudong.permission.model.Test();
+        test.setTestTest("test");
+        testMapper.insert(test);
+    }
 }
