7ff0da6781dea7f7a0d5e9509f419c2ff9ded54d
zoudong <zhoud006@udduo.com>
2018-06-14 12:01:11 +0800

    ehcache 集成

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,5 +1,6 @@
 import com.Application;
 import com.zoudong.permission.mapper.TestMapper;
+import com.zoudong.permission.utils.EhcacheUtil;
 import com.zoudong.permission.utils.RedisUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.runner.RunWith;
@@ -23,31 +24,50 @@ public class Test {
     private TestMapper testMapper;
     @Autowired
     private RedisUtils redisUtils;
+
     @org.junit.Test
-    public void testMybatis(){
-        com.zoudong.permission.model.Test test=new com.zoudong.permission.model.Test();
+    public void testMybatis() {
+        com.zoudong.permission.model.Test test = new com.zoudong.permission.model.Test();
         test.setTest("test");
         testMapper.insert(test);
     }
 
     @org.junit.Test
-    public void testRedis(){
-        String key="zoudong";
+    public void testRedis() {
+        String key = "zoudong";
         //如果缓存存在
         boolean hasKey = redisUtils.exists(key);
-        if(hasKey){
+        if (hasKey) {
             //获取缓存
-            Object object =  redisUtils.get(key);
-            log.info("从缓存获取的数据"+ object);
-        }else{
+            Object object = redisUtils.get(key);
+            log.info("从缓存获取的数据" + object);
+        } else {
             //从DB中获取信息
             log.info("从数据库中获取数据");
             List<com.zoudong.permission.model.Test> tests = testMapper.selectAll();
             //数据插入缓存（set中的参数含义：key值，user对象，缓存存在时间10（long类型），时间单位）
-            redisUtils.set(key,tests,10L, TimeUnit.SECONDS);
-            log.info("数据插入缓存"+tests.toString());
+            redisUtils.set(key, tests, 10L, TimeUnit.SECONDS);
+            log.info("数据插入缓存" + tests.toString());
         }
 
     }
+
+
+    @org.junit.Test
+    public void testEhcache() {
+        String key = "zoudong";
+        //如果缓存存在
+        List<com.zoudong.permission.model.Test> tests = testMapper.selectAll();
+        EhcacheUtil.getInstance().put("permission", key, tests);
+        if (EhcacheUtil.getInstance().get("permission", key) != null) {
+            Object value = EhcacheUtil.getInstance().get("permission", key);
+            log.info("ehcache value:{}", value);
+        }else{
+            log.info("not fund ehcache value!");
+        }
+
+
+    }
+
 
 }
