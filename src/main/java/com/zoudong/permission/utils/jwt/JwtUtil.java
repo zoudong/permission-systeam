aee63fa55cdd057e5e091366108f0f012fe531c5
zoudong <zhoud006@udduo.com>
2018-06-15 12:02:17 +0800

    jwt shiro test

---
+++ src/main/java/com/zoudong/permission/utils/jwt/JwtUtil.java
@@ -0,0 +1,73 @@
+package com.zoudong.permission.utils.jwt;
+
+import com.auth0.jwt.internal.org.apache.commons.codec.binary.Base64;
+import com.zoudong.permission.constant.JwtConstant;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    /**
+     * 由字符串生成加密key
+     *
+     * @return
+     */
+    public SecretKey generalKey() {
+        String stringKey = JwtConstant.JWT_SECRET;
+        byte[] encodedKey = Base64.decodeBase64(stringKey);
+        SecretKey key = new SecretKeySpec(encodedKey, 0, encodedKey.length, "AES");
+        return key;
+    }
+
+    /**
+     * 创建jwt
+     *
+     * @param id
+     * @param subject
+     * @param ttlMillis
+     * @return
+     * @throws Exception
+     */
+    public String createJWT(String id, String subject, long ttlMillis) throws Exception {
+
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+        SecretKey key = generalKey();
+        JwtBuilder builder = Jwts.builder()
+                .setId(id)
+                .setIssuedAt(now)
+                .setSubject(subject)
+                .signWith(signatureAlgorithm, key);
+        if (ttlMillis >= 0) {
+            long expMillis = nowMillis + ttlMillis;
+            Date exp = new Date(expMillis);
+            builder.setExpiration(exp);
+        }
+        return builder.compact();
+    }
+
+    /**
+     * 解密jwt
+     *
+     * @param jwt
+     * @return
+     * @throws Exception
+     */
+    public Claims parseJWT(String jwt) throws Exception {
+        SecretKey key = generalKey();
+        Claims claims = Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(jwt).getBody();
+        return claims;
+    }
+
+
+}
---
+++ src/main/java/com/zoudong/permission/utils/shiro/TokenSubjectFactory.java
@@ -0,0 +1,14 @@
+package com.zoudong.permission.utils.shiro;
+
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.subject.SubjectContext;
+import org.apache.shiro.web.mgt.DefaultWebSubjectFactory;
+
+public class TokenSubjectFactory extends DefaultWebSubjectFactory {
+
+    public Subject createSubject(SubjectContext context) {
+        //不创建 session
+        context.setSessionCreationEnabled(false);
+        return super.createSubject(context);
+    }
+}
