63349ee682a086309ce4b7829811f087b95f6cbb
zoudong <zhoud006@udduo.com>
2018-06-15 14:13:16 +0800

    shiro session从header里面取

---
+++ src/main/java/com/zoudong/permission/utils/shiro/MySessionManager.java
@@ -0,0 +1,38 @@
+package com.zoudong.permission.utils.shiro;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.authz.UnauthorizedException;
+import org.apache.shiro.web.servlet.ShiroHttpServletRequest;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.apache.shiro.web.util.WebUtils;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.Serializable;
+
+public class MySessionManager extends DefaultWebSessionManager {
+
+    private static final String AUTHORIZATION = "Authorization";
+
+    private static final String REFERENCED_SESSION_ID_SOURCE = "Stateless request";
+
+    public MySessionManager() {
+        super();
+    }
+
+    @Override
+    protected Serializable getSessionId(ServletRequest request, ServletResponse response) {
+        String token = WebUtils.toHttp(request).getHeader(AUTHORIZATION);
+        //如果请求头中有 Authorization 则其值为sessionId  
+        if (!StringUtils.isEmpty(token)) {
+            request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID_SOURCE, REFERENCED_SESSION_ID_SOURCE);
+            request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID, token);
+            request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID_IS_VALID, Boolean.TRUE);
+            return token;
+        } else {
+            throw new UnauthorizedException();
+            //否则按默认规则从cookie取sessionId  
+            //return super.getSessionId(request, response);
+        }
+    }
+}  
