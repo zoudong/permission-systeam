5e69380eb472a67dbc683b3cecdc5c3f016b12ac
zoudong <zhoud006@udduo.com>
2018-06-20 16:02:16 +0800

    shiro filter

---
+++ src/main/java/com/zoudong/permission/config/Filter.java
@@ -0,0 +1,22 @@
+package com.zoudong.permission.config;
+
+import com.zoudong.permission.config.shiro.filter.AccessTokenFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zd
+ * @description class
+ * @date 2018/6/20 14:52
+ */
+@Configuration
+public class Filter {
+    /*@Bean
+    public FilterRegistrationBean myFilter() {
+        FilterRegistrationBean myFilter = new FilterRegistrationBean();
+        myFilter.addUrlPatterns("/*");
+        myFilter.setFilter(new AccessTokenFilter());
+        return myFilter;
+    }*/
+}
---
+++ src/main/java/com/zoudong/permission/config/shiro/filter/AccessTokenFilter.java
@@ -0,0 +1,42 @@
+package com.zoudong.permission.config.shiro.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.web.filter.authz.AuthorizationFilter;
+import sun.misc.BASE64Decoder;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+public class AccessTokenFilter extends AuthorizationFilter {
+
+    @Override
+    protected boolean isAccessAllowed(ServletRequest servletRequest, ServletResponse servletResponse, Object o) throws Exception {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String auth = (String) httpServletRequest.getAttribute("Authorization");
+        System.out.println("auth encoded in base64 is " + getFromBASE64(auth));
+        if ((auth != null) && (auth.length() > 6)) {
+            auth = auth.substring(6, auth.length());
+            String decodedAuth = getFromBASE64(auth);
+            System.out.println("auth decoded from base64 is " + decodedAuth);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private String getFromBASE64(String s) {
+        if (s == null)
+            return null;
+        BASE64Decoder decoder = new BASE64Decoder();
+        try {
+            byte[] b = decoder.decodeBuffer(s);
+            return new String(b);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+
+}
